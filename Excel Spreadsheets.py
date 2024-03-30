187a5be0d98ec68ce66cace796f3a920f0a26d66
BANU PRIYA D <139879880+priyad1221@users.noreply.github.com>
2024-03-30 20:15:24 +0530

    Add files via upload

---
+++ Excel Spreadsheets.py
@@ -0,0 +1,34 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+wb=xl.load_workbook('Book1.xlsx')
+sheet = wb['Sheet1']
+
+#   to get a particular cell value and to print in the terminal
+#cell = sheet['a1']
+#cell = sheet.cell(1,1)
+#print(cell.value)
+
+#   to get and print the maximum row in a sheet
+#print(sheet.max_row)
+
+for row in range(2,sheet.max_row+1):
+    #print(row)
+    cell=sheet.cell(row,3)
+    #print(cell.value)
+    corrected_price=cell.value * 0.9
+    #print(corrected_price)
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value = corrected_price
+
+
+
+values = Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart,'e2')
+wb.save('Book2.xlsx')
